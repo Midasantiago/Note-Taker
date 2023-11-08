9e5a70f22dc1e6abbdbf6716d99b4cf2810caed7
Bixey <mida.santiago98@gmail.com>
2023-11-07 21:53:16 -0500

    Completed Task

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -2,8 +2,26 @@ const notes = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
-notes.get('/api/notes', (req, res) => {
+notes.get('/notes', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post('/notes', (req, res) => {
+    console.log(req.body);
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuid()
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json('Note added successfully');
+    } else {
+        res.errored('Error in adding note');
+    }
 });
 
 module.exports = notes;
